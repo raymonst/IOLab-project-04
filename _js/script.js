b0c1ede5535aa81b38a02a6d924612d413a20ba7
JT Huang <jthuang@ischool.berkeley.edu>
2012-11-28 21:33:53 -0800

    Instagram tag search

--- _js/script.js
+++ _js/script.js
@@ -25,9 +25,28 @@ var twitter = {
 var instagram = {
 
     //----------------------------------------------------------------------------------------------------------
-    init : function() {
+    get : function(tag) {
+       // get instagram pics with 'tag' tagged and append to "#instagramImages"
+       var api_key = "access_token=215516.f2e0088.4d29740528bf40b08b1e976bc41ac66d"
+       var url = "https://api.instagram.com/v1/tags/"
+       var recent_tag = "/media/recent"
+       
+       console.log(url + tag + recent_tag + '?' + api_key);
+       $.getJSON(url + tag + recent_tag + '?' + api_key +'&callback=?',
+         function(json) {
+            console.log(json);
+            $(json.data).each(function(index) {
+               //console.log(this);
+               var html_str = '<a href="' + this.link + '">' +
+                                 '<img src="' + this.images.standard_resolution.url + '"' +
+                                 //' title="' + this.caption.text + '"' + 
+                                 ' class="label label-info" width = 250px/>' +
+                              '</a>';
+               //console.log(html_str);
+               $('<li style="float:left;list-style-type: none"></li>').html(html_str).appendTo('#instagramImages');
+            });
+         });
     }
-
 }
 
 
@@ -48,5 +67,6 @@ var tumblr = {
 $(document).ready(function() {
 
     func.init();
+    instagram.get("snow");
 
 });
