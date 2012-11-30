1f4871cb984a01842cd272f9298b76975eb735c5
Raymon Sutedjo-The <raymonst@ischool.berkeley.edu>
2012-11-30 13:58:02 -0800

    first try - form

--- _js/script.js
+++ _js/script.js
@@ -2,6 +2,35 @@ var func = {
 
     //----------------------------------------------------------------------------------------------------------
     init : function() {
+	    func.form();
+    },
+
+    //----------------------------------------------------------------------------------------------------------
+    form : function() {
+	    var count = 0
+	    $("#content form").on("submit", function() {
+		    count++;
+		    switch(count) {
+		    case 1:
+		    	instagram.get("rain");
+		    	//alert("show instagram");
+		    	return false;
+		    	break;
+		    case 2:
+		    	alert("show tumblr");
+		    	break;
+		    case 3:
+		    	alert("show tweets");
+		    	break;
+		    case 4:
+		    	alert("show answer");
+		    	break;
+		    default:
+		    	alert("nothing");
+		    	break;
+		    }
+		    return false;
+	    });
     }
 
 }
@@ -26,27 +55,51 @@ var instagram = {
 
     //----------------------------------------------------------------------------------------------------------
     get : function(tag) {
-       // get instagram pics with 'tag' tagged and append to "#instagramImages"
-       var api_key = "access_token=215516.f2e0088.4d29740528bf40b08b1e976bc41ac66d"
-       var url = "https://api.instagram.com/v1/tags/"
-       var recent_tag = "/media/recent"
-       
-       console.log(url + tag + recent_tag + '?' + api_key);
-       $.getJSON(url + tag + recent_tag + '?' + api_key +'&callback=?',
-         function(json) {
-            console.log(json);
-            $(json.data).each(function(index) {
-               //console.log(this);
-               var html_str = '<a href="' + this.link + '">' +
-                                 '<img src="' + this.images.standard_resolution.url + '"' +
-                                 //' title="' + this.caption.text + '"' + 
-                                 ' class="label label-info" width = 250px/>' +
-                              '</a>';
-               //console.log(html_str);
-               $('<li style="float:left;list-style-type: none"></li>').html(html_str).appendTo('#instagramImages');
-            });
-         });
-    }
+	    // get instagram pics with 'tag' tagged and append to "#instagramImages"
+	    var api_key = "access_token=215516.f2e0088.4d29740528bf40b08b1e976bc41ac66d"
+	    var main_url = "https://api.instagram.com/v1/tags/"
+	    var recent_tag = "/media/recent"
+
+	    console.log(main_url + tag + recent_tag + '?' + api_key);
+	    $.getJSON(main_url + tag + recent_tag + '?' + api_key + '&callback=?', function(json) {	
+		    //console.log(json);
+		    var count = 0;
+		    $(json.data).each(function(index) {
+			    //console.log(this);
+			    if (count < 5) {
+				    var html_str = '<a href="' + this.link + '">' + '<img src="' + this.images.standard_resolution.url + '"' 
+				    	//+ ' title="' + this.caption.text + '"' 
+				    	+ ' class="label label-info" width="100" height="100"/>' + '</a>';
+				    //console.log(html_str);
+				    $('<li></li>').html(html_str).appendTo('#instagram');
+			    }
+			    count++;
+			});
+			$("#instagram").show();
+		});
+
+/*
+		$.ajax({
+			url: main_url + tag + recent_tag + '?' + api_key + '&callback=?',
+			success: function(json) {
+		    	var count = 0;
+		    	$(json.data).each(function(index) {
+			    	//console.log(this);
+			    	if (count < 5) {
+				    	var html_str = '<a href="' + this.link + '">' + '<img src="' + this.images.standard_resolution.url + '"' + ' title="' + this.caption.text + '"' + ' class="label label-info" width="100" height="100"/>' + '</a>';
+				    	//console.log(html_str);
+				    	$('<li></li>').html(html_str).appendTo('#instagram');
+				    }
+				    count++;
+				});
+				alert('Load was performed.');
+			}
+		});
+*/
+
+
+	}
+
 }
 
 
@@ -67,6 +120,5 @@ var tumblr = {
 $(document).ready(function() {
 
     func.init();
-    instagram.get("snow");
 
 });
