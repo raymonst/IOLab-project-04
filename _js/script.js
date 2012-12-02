c6bd1a75488ca34eab95822e1ad72aee18ecebf6
Chan Kim <chan.berkeley@gmail.com>
2012-12-01 16:02:11 -0800

    add game logic to script.js and add several <div> in index.html

--- _js/script.js
+++ _js/script.js
@@ -1,30 +1,78 @@
+var history_input = [];
+var hint_counter = 1;
+var answer = "rain";
+var life = 5;
+var duplicate_flag =false;
+
 var func = {
 
     //----------------------------------------------------------------------------------------------------------
     init : function() {
-	    func.form();
+	    func.form(hint_counter,answer);
     },
+ 
+	//----------------------------------------------------------------------------------------------------------
+	validate : function(input, answer){
+		console.log(duplicate_flag);
+		//prevent duplicate input
+		for (var i=0;i<history_input.length ; i++){
+			if (input ==history_input[i] && life <5){ 
+				duplicate_flag =true;
+			}
+			else duplicate_flag= false;
+		}	
+		if (duplicate_flag)	{
+			$('#msg').html("<p>You typed the previous answer again. Please type different input.</p>");
+		}
+		else {
+			if (input == answer){
+				//load ending screen function
+				msg ="Congratulation!!";
+				func.ending_screen(msg);
+			}
+			else {
+				// still life left
+				if (life >0){
+					$('#guess-tag').html('');
+					// try again message
+					$('#msg').html("<span> You've got wrong answer. Please try again.</span>");
+					// decrease life, then show the remained life
+					life --;
+					$('#life').html("<span>You have "+life +"  life left.</span>");
+					// load "more hint" btn & "show answer" btn
+					$('#button').html("<button id='hints'>See more hints</button> <br> <button id='answer'>show answer</button>");
+					// show the history of the input
+					$('#history_input').append("<span> "+input+" </span>");
+					history_input.push(input);
+					console.log(history_input);
+					// (optional) how close the input was
+				}
+				else {
+					msg = "Game Over. Wanna try again?"
+					func.ending_screen(msg);
+				}
+			}
+		}// duplicate check
+	},
 
     //----------------------------------------------------------------------------------------------------------
-    form : function() {
+    form : function(count, answer) {
 
-
-	    trending_topics = twitter.get();
-	    var trend_count = 0;
-
-	    var count = 0;
-	    $("#content form").on("submit", function() {
-
-		    if ($("#guess-tag").val() != trending_topics[trend_count]) {
-			    count++;
-		    };
-
-		    switch(count) {
+//	    trending_topics = twitter.get();
+//	    var trend_count = 0;
+//
+//	    var count = 0;
+//	    $("#content form").on("submit", function() {
+//
+//		    if ($("#guess-tag").val() != trending_topics[trend_count]) {
+//			    count++;
+//		    };
+		  switch(count) {
 		    case 1:
-		    	instagram.get(trending_topics[trend_count]);
+		    	instagram.get(answer);
 		    	break;
 		    case 2:
-		    	tumblr.get(trending_topics[trend_count]);
+		    	tumblr.get(answer);
 		    	break;
 		    case 3:
 		    	alert("show tweets");
@@ -38,10 +86,27 @@ var func = {
 		    default:
 		    	alert("correct!");
 		    	break;
-		    }
-		    return false;
-	    });
-    }
+		  }
+		  return false;
+    },
+
+	//----------------------------------------------------------------------------------------------------------
+	ending_screen : function(msg){
+		$('#msg').html("<p>"+msg+"</p>");
+		$('#msg').append("<a href='javascript:document.location.reload();'>Retry the Game</a>");
+		$('#life').html('');
+		$('#history_input').html('');
+		$('#button').html('');
+
+		// initiate default variables
+		hint_counter = 1;
+		life =5;
+		duplicate_flag=false;
+		for (var i=0;i=history_input.length;i++){
+			history_input.pop();
+		}
+	}
+
 
 }
 
@@ -174,7 +239,30 @@ var tumblr = {
 
 //--------------------------------------------------------------------------------------------------------------
 $(document).ready(function() {
-
     func.init();
-
 });
+
+//--------------------------------------------------------------------------------------------------------------
+// click submit button
+$("#content form").live("submit", function() {
+			var input =$("#guess-tag").val();
+			console.log(input);
+			func.validate(input, answer);
+	return false;
+ });
+
+//--------------------------------------------------------------------------------------------------------------
+// click "show more hints"
+$("#hints").live("click", function() {
+			console.log(hint_counter);
+			hint_counter ++;
+			func.form(hint_counter, answer);
+	return false;
+ });
+
+//--------------------------------------------------------------------------------------------------------------
+// click "show answer"
+$("#answer").live("click", function() {
+			msg ="We're sorry for making you blown up. The answer is "+answer +".";
+			func.ending_screen(msg);
+ });
